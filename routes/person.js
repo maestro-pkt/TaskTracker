a20ce9186145def09fdfdaeea24deadd42d60d66
maestro-pkt <pesteph@gmail.com>
2014-05-14 19:09:02 -0400

    Clean up people view

--- routes/person.js
+++ routes/person.js
@@ -31,19 +31,16 @@ exports.view = function(req, res) {
 }
 
 exports.viewPerson = function(req, res) {
-  /*
-Test.find({
-      $and: [
-          { $or: [{a: 1}, {b: 1}] },
-          { $or: [{c: 1}, {d: 1}] }
-      ]
-  }, function (err, results) {
-      ...
-  }
-   */
   
   Person.find({}, function(err, ppl) {
     if (!err) {
+      var person = {};
+      _.find( ppl, function( value ){
+        if( value._id == req.param('_id')) {
+          person = value;
+        }
+      });
+      
       Item.find({ ppl: req.param('_id')}, function( err, items ) {
         console.log( items );
         var queryStr = {};
@@ -68,12 +65,9 @@ Test.find({
             } 
             
             _.each( card.items, function( element, index, allItems ){
-              console.log( 'foreach');
-              console.log( element );
-              console.log( index );
-              
-              // var even = _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
-              //=> 2
+              //console.log( 'foreach');
+              //console.log( element );
+              //console.log( index );
               
               _.find( element.ppl, function( value ){
                 if( value == req.param('_id')) {
@@ -82,33 +76,7 @@ Test.find({
                 }
               });
               
-              /*console.log( 'checking array');
-              console.log( element.ppl );
-              console.log( 'for ');
-              console.log( req.param('_id') ) ;
-              if (element.ppl instanceof Array) {
-                console.log( 'it is an array');
-              }
-              console.log("Contains 25?: " + _.contains([1,3,16,25,44], 25)); 
-console.log( "evaluated: " + _.contains( element.ppl, req.param('_id') )) ;
-console.log( "evaluated2: " + _.contains( [ "536ab03a7b405db02f904a12", "536ab03a7b405db02f904a13", "tetris" ], "536ab03a7b405db02f904a12")) ;
-
-              if( _.contains( element.ppl, req.param('_id') ) ) {
-                console.log( 'found a match');
-                items.push( { title: card.title, _id:card._id, itemText: element.text });
-              }
-              */
             }); 
-              // where item id's from array a[] is an id in this card.items array
-              //card.items
-              //
-            /*
-            Iterates over a list of elements, yielding each in turn to an iterator function. 
-            The iterator is bound to the context object, if one is passed. Each invocation of 
-            iterator is called with three arguments: (element, index, list). If list is a JavaScript 
-            object, iterator's arguments will be (value, key, list). Delegates to the native forEach 
-            function if it exists, and returns the original list for chaining.
-            */
             
            
           }
@@ -116,7 +84,9 @@ console.log( "evaluated2: " + _.contains( [ "536ab03a7b405db02f904a12", "536ab03
           console.log( owners );
           console.log( 'items');
           console.log( items )
+
           res.render('people', {
+            person: person,
             owner: owners,
             items: items,
             ppl : ppl
