735f5ce3813e57684a05c2dc54a0ab97895a0559
maestro-pkt <pesteph@gmail.com>
2014-05-18 17:40:01 -0400

    Enabling SSL / PKI

---
+++ mayUse/Getting Cirrius  Securing Node.js and Express with SSL Client-Authentication_files/jquery.popeye-2.0.2.min.js
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="de" class="no-js">
+<head>
+	<meta charset="utf-8">
+	<title>Error 404 | Freelance-Webdesigner &amp; Konzepter Christoph Schüßler</title>
+	<meta name="description" content="Christoph Schüßler gestaltet und programmiert User-Interfaces im Web" />
+	<meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" name="viewport">
+	<meta name="google-site-verification" content="y3eQhoi3ZZf-mjnU2uH-GZbXwIFrCgRVYgAd66pZs9k" />
+	<link rel="Shortcut Icon" type="image/icon" href="http://herrschuessler.de/favicon.ico">
+	<link href='http://fonts.googleapis.com/css?family=Domine:700|Oswald:700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+	<script src="http://herrschuessler.de/js/modernizr.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="http://herrschuessler.de/css/style.css">
+	<script>
+		var gaProperty = 'UA-266037-30';
+		var disableStr = 'ga-disable-' + gaProperty;
+		if (document.cookie.indexOf(disableStr + '=true') > -1) {
+			window[disableStr] = true;
+		}
+		function gaOptout() {
+			document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+			window[disableStr] = true;
+		}
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-266037-30', 'herrschuessler.de');
+		ga('set', 'anonymizeIp', true);
+		ga('send', 'pageview');
+	</script>
+</head>
+
+<body>
+	<article>
+		<header class="container">
+			<div class="logo-wrapper" id="logo">
+				<div class="logo"></div>
+			</div>
+			<h1>Error 404</h1>
+		</header>
+		<section class="about container">
+			<p><a class="link" href="http://herrschuessler.de/">Weitergehen</a>, hier gibt es nichts zu sehen!</p>
+		</section>
+	</article>
+</body>
+</html>
